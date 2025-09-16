cd1e9ca81add13d9dad61434f67f04739be20e0b
Yavuz-Adiguzel <adiguzel.yavuzz@gmail.com>
2025-09-16 09:53:04 +0300

    Öğrenci listeleme ve kaydetme aktif.

---
+++ MVC/Controllers/OgrencilerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class OgrencilerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public OgrencilerController (IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient("OkulApi");
+            var response = await client.GetFromJsonAsync<List<OgrenciCreateDto>>("Ogrenciler");
+            return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OgrenciCreateDto ogrenci)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ogrenci);
+            }
+
+            var client = _httpClientFactory.CreateClient("OkulApi");
+            var response = await client.PostAsJsonAsync("Ogrenciler/ogrenci", ogrenci);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Öğrenci eklenemedi");
+            return View(ogrenci);
+        }
+    }
+}
---
+++ MVC/Models/OgrenciCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC.Models
+{
+    public class OgrenciCreateDto
+    {
+        public string AdiSoyadi { get; set; }
+    }
+}
